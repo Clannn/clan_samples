8d3920e35befaf54778530c3ad98b4abd035113d
浮云 <1335732406@qq.com>
2023-02-24 13:24:40 +0800

    add resource_caching and update descriptor_pool

--- framework/core/descriptor_set.cpp
+++ framework/core/descriptor_set.cpp
@@ -1,1 +1,7 @@
 #include "descriptor_set.h"
+
+#include "common/logging.h"
+#include "common/resource_caching.h"
+#include "descriptor_pool.h"
+#include "descriptor_set_layout.h"
+#include "device.h"
